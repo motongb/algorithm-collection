d3923cd97c813ca6cac9288972e59e634e7a3df3
motb <422584753@qq.com>
2019-08-25 11:30:28 +0800

    init 两数相加

---
+++ src/base/BaseAlgorithm.java
@@ -0,0 +1,76 @@
+package base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author motb
+ * @date 2019/8/25 11:04
+ * @description: 基础算法题目
+ */
+public class BaseAlgorithm {
+    /**
+     * 两数之和
+     * <p>
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * -------------------------
+     * 示例:
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * <p>
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     * -------------------------
+     */
+
+    /**
+     * 暴力法
+     * 暴力法很简单，遍历每个元素 xx，并查找是否存在一个值与 target - xtarget−x 相等的目标元素。
+     * 时间复杂度：O(n2)
+     * 空间复杂度：O(1)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 两遍哈希表
+     * 在第一次迭代中，我们将每个元素的值和它的索引添加到表中。然后，在第二次迭代中，我们将检查每个元素所对应的目标元素（target - nums[i]target−nums[i]）
+     * 是否存在于表中。注意，该目标元素不能是 nums[i]nums[i] 本身！
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     * 不考虑数组重复值
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            Integer index = map.get(complement);
+            if (index != null && index != i) {
+                return new int[]{i, index};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+}
